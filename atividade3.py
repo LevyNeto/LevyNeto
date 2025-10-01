85da37930ef698cb9fd5c799de3ad7aae157fc79
Levy Neto <fernandesnetolevy@gmail.com>
2025-10-01 17:24:26 -0300

    Delete atividade3.py

--- atividade3.py
+++
@@ -1,6 +0,0 @@
-pergunta1=int(input ("Digite o primeiro número"))
-pergunta2=int (input("digite o segundo nùmero"))
-if (pergunta1>pergunta2):
-    print(f"{pergunta1} o maior número")
-else:
-    print(f"{pergunta2} é o maior número")
