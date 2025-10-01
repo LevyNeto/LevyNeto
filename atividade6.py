53791bc717a03446b9e5ca9589e5b2d4def75ab8
Levy Neto <fernandesnetolevy@gmail.com>
2025-10-01 17:24:44 -0300

    Delete atividade6.py

--- atividade6.py
+++
@@ -1,6 +0,0 @@
-numero1= int (input("digite o primeiro numero"))
-numero2= int (input("digite o segundo numero"))
-if (numero2==numero1):
-    print("esses numeros são iguais ")
-else:
-    print("esses numeros são diferentes")
